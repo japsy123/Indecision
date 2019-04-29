e87cb6c193daf88606e27cb23264230acfe9f468
Jal Panchal <jal08panchal@gmail.com>
2019-04-29 06:27:18 -0700

    Adding template2 for JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,10 +3,30 @@
 console.log("app is running");
 
 var template = React.createElement(
-  "p",
-  null,
-  " This is JSX"
+    "p",
+    null,
+    " This is JSX!!"
+);
+
+var template2 = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "p",
+        null,
+        " Jal "
+    ),
+    React.createElement(
+        "p",
+        null,
+        " Panchal "
+    ),
+    React.createElement(
+        "p",
+        null,
+        "25"
+    )
 );
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 console.log("app is running")
 
-var template = <p> This is JSX</p>
+var template = <p> This is JSX!!</p>
+
+var template2 = (
+    <div> 
+        <p> Jal </p>
+        <p> Panchal </p>
+        <p>25</p>
+    </div>
+);
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot)
+ReactDOM.render(template2, appRoot)
