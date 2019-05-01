f523597bdf4e63d2b1ef5980ac13f27f01a15502
Jal Panchal <jal08panchal@gmail.com>
2019-05-01 05:55:23 -0700

    Creating template for visibility

---
+++ src/playground/build-it.js
@@ -0,0 +1,14 @@
+
+var appRoot = document.getElementById('app');
+
+const render = () => {
+
+    const template = (
+
+        <p> Visibility Toggle</p>
+    )
+
+    ReactDOM.render(template, appRoot)
+
+    
+}
