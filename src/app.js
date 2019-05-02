c7e53d70fb582577f8e3a6bf6ab4a7e3bd1b3116
Jal Panchal <jal08panchal@gmail.com>
2019-05-02 06:25:37 -0700

    Creating Header component

--- src/app.js
+++ src/app.js
@@ -1,50 +1,12 @@
+class Header extends React.Component {
 
-console.log("app is running")
-
-var template = <p> This is JSX!!</p>
-var appObject = {
-    title: "Root",
-    subTitle: "RootSub",
-    options: ["One", "Two"]
-}
-
-const onFormSubmit = (e) => {
-    e.preventDefault();
-
-    const option = e.target.elements.option.value;
-
-    if(option) {
-        appObject.options.push(option);
-        e.target.elements.option.value = "";
-        render();
+    render() {
+        return (
+            <div>
+            <h1>Indecision  </h1>
+            <p>Put your hands in front of computer</p>
+            </div>
+        )
     }
-}
-var appRoot = document.getElementById('app');
-
-
-const render = () => {
-
-    var template2 = (
-        <div> 
-            <p> {appObject.title} </p>
-            {appObject.subTitle && <p>{appObject.subTitle}</p> } 
-            <p>{ appObject.options.length >0 ? "Here are your options" : "No options" }
-            </p>
-            <p>{appObject.options.length}</p>
-            <ol>
-               {appObject.options.map((option)=>{
-                    return <li key={option}>Option: {option}</li>
-               })}
-            </ol> 
-            <form onSubmit= {onFormSubmit}>
-            <input type="text" name="option"/>
-            <button>Add options</button>
-            </form>
-        </div>
-    );
-
-    ReactDOM.render(template2, appRoot)
 
 }
-
-render();
