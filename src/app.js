7a2f1de4784b245f8441c9d54506df9702a872a5
Jal Panchal <jal08panchal@gmail.com>
2019-05-10 12:14:59 -0700

    Importing react,react-dom in app

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
-console.log("Running");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const template = <p> Hiii</p>;
+
+ReactDOM.render(template, document.getElementById("app"));
