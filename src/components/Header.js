f5373767edb71c140f57dd7a06a13307de6285d8
Jal Panchal <jal08panchal@gmail.com>
2019-05-12 09:18:32 -0700

    Changing small text in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = props => {
   return (
     <div>
       <h1>Indecision {props.subTitle}</h1>
-      <p>Put your hands in front of computer</p>
+      <p>Put your hands front of computer</p>
     </div>
   );
 };
